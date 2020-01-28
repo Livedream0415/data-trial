dcf5c4b8f0024ff257159e0909e09690216fc5f0
LiveDream0415 <livedream0415@outlook.com>
2020-01-28 13:02:28 -0500

    Implement format, sort, offset and limit

--- components/server/api/urls.py
+++ components/server/api/urls.py
@@ -6,8 +6,8 @@ from api import views
 # Wire up our API using automatic URL routing.
 router = routers.DefaultRouter()
 
-router.register(r'users', views.UserViewSet)
-router.register(r'messages', views.MessageViewSet)
+router.register(r'users', views.UserViewSet, basename='userView')
+router.register(r'messages', views.MessageViewSet, basename='messageView')
 
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
--- components/server/api/views.py
+++ components/server/api/views.py
@@ -10,15 +10,44 @@ class UserViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows groups to be viewed or edited.
     """
-    queryset = User.objects.all()
     serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+    def list(self, request):
+        index = int(self.request.GET.get("index", self.queryset.count()-1))
+        vector = int(self.request.GET.get("vector", -10))
+        sort = self.request.GET.get("sort", "username")
+
+        if vector < 0:
+            vector = -vector
+            index = index - vector + 1
+        if index < 0:
+            vector += index
+            index = 0
+        if sort == "username":
+            sort = "-username"
+
+        user_list = self.queryset.order_by(sort)[index:index+vector]
+        serialized_user_list = UserSerializer(user_list, many=True)
+        return JsonResponse({
+            "total_length": self.queryset.count(),
+            "array": serialized_user_list.data
+        })
 
     def create(self, request):
-        user = UserSerializer(data=request.data)
-        user.timestamp = parse_datetime(request.data['timestamp'])
-        if user.is_valid():
-            user.save()
-        return JsonResponse({"id": user.data["id"]})
+        is_existed = User.objects.filter(
+            username=request.data['username']
+        ).count()
+        if is_existed == 0:
+            user = UserSerializer(data=request.data)
+            user.timestamp = parse_datetime(request.data['timestamp'])
+            if user.is_valid():
+                user.save()
+            return JsonResponse({'id': user.data['id']}, status=200)
+        else:
+            return JsonResponse({
+                'error': 'username is already in use'
+            }, status=409)
 
 
 class MessageViewSet(viewsets.ModelViewSet):
@@ -27,6 +56,24 @@ class MessageViewSet(viewsets.ModelViewSet):
     """
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
+
+    def list(self, request):
+        index = int(self.request.GET.get("index", self.queryset.count()-1))
+        vector = int(self.request.GET.get("vector", -10))
+
+        if vector < 0:
+            vector = -vector
+            index = index - vector + 1
+        if index < 0:
+            vector += index
+            index = 0
+
+        message_list = self.queryset[index:index+vector]
+        serialized_message_list = MessageSerializer(message_list, many=True)
+        return JsonResponse({
+            "total_length": self.queryset.count(),
+            "array": serialized_message_list.data
+        })
 
     def create(self, request):
         message = MessageSerializer(data=request.data)
